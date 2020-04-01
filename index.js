04b410f5cd93d56cbdc60ff3a5ccb77178b73283
SpencerBlum <spencerblum@gmail.com>
2020-04-01 10:00:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+
+let element = document.querySelector("p#greeting");
+element.innerHTML = 'Hello, DOM!'
+
+let main = document.getElementById("main")
+
+main.remove()
+
+let newHeader = document.createElement("h1")
+
+newHeader.id = "victory"
+
+newHeader.innerHTML = "spencer is the champion"
+
+
