34bab575450a1d201a0775ef6ebae770af2011a7
arieldavis22 <aadavisbusiness@gmail.com>
2020-04-01 16:15:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let bodyMain = document.querySelector("#main");
+bodyMain.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Apple is the champion"
